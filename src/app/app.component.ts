0ef395c2923da36ed81dbc8bb8fb4191f8ce4bcd
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-03-28 01:13:14 +0900

    기본 설명

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // our own html tag we assign so to say
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular';
