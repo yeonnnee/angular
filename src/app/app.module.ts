95f4823a9c263f914a41942e2f3fc1970a43c8f3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-04-09 22:24:31 +0900

    실수로 지운 브라우저모듈 import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainPage } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
+    MainPage
+  ],
+  imports: [
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-
 })
 export class AppModule {}
