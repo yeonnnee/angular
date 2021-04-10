1ec4652ee6299557ac05b3a57f75ef9aa52055bc
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-04-10 22:40:35 +0900

    버튼 클릭 후 input에 변화 생길시 커피그림 자동 업데이트 되는 현상 수정 완료

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,21 +10,16 @@ import { CoffeeService } from "../coffee.service";
 export class MainPage {
   coffeeStatus: boolean=false;
   quantity: Array<number> = [];
+  inputVal: number;
 
   constructor(public coffeeService: CoffeeService){}
 
   setQty(e: any) { 
-    const val = e.target.value;
-    this.coffeeService.setQty(+val);
-    // if (this.quantity.length > 0) {
-    //   this.coffeeService.setQty(+val);
-    // } else {
-    //   this.coffeeService.setQty(+val);
-    // }
-    
+    this.inputVal = e.target.value;
   }
 
   getOrder() {
+    this.coffeeService.setQty(+this.inputVal);
     this.quantity = this.coffeeService.getQty();
     if (this.quantity.length > 0) {
       this.coffeeStatus = true;
