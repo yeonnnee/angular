337e61cffe1991ec5b044daeb10813d8637083d5
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-04-10 17:14:27 +0900

    커피 컴포넌트 생성 및 CUSTOM_ELEMENTS_SCHEMA 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoffeeComponent } from './coffee/coffee.component';
 import { MainPage } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPage
+    MainPage,
+    CoffeeComponent
   ],
   imports: [
     BrowserModule
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/coffee/coffee.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-coffee',
+  templateUrl: './coffee.component.html',
+  styleUrls:['./coffee.component.css']
+})
+
+export class CoffeeComponent {}
