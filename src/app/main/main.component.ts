14a51c3ec88e952cb71efaa62137da3ae1b6f928
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-05-09 19:13:17 +0900

    pattern validation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
-import { Component } from "@angular/core";
-import { FormControl,FormBuilder,Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl,FormBuilder,Validators, FormGroup } from '@angular/forms';
 import { CoffeeList, OrderList } from "../model/coffee.model";
-
 
 @Component({
   selector: 'app-main',
@@ -9,19 +8,28 @@ import { CoffeeList, OrderList } from "../model/coffee.model";
   styleUrls:['./main.component.css']
 })
 
-export class MainPage {
-
+export class MainPage implements OnInit {
+  nameValidator(name) {
+    console.log(name)
+    return (control): {[key: string]: any} | null => {
+      // const forbidden = name.test(control.value);
+      return name ? {forbiddenName: {value: control.value}} : null;
+    };
+  }
+ 
   /*
   폼 컨트롤의 초기값만 지정한다면 배열을 사용하지 않아도 됩니다. 
   하지만 폼 컨트롤에 유효성 검사기를 지정하려면 프로퍼티 값에 배열을 사용해야 합니다.
   이 때 동기 유효성 검사기는 두번째 인자로, 
   비동기 유효성 검사기는 세번째 인자로 지정합니다.
   */
-  orderForm = this.fb.group({
-    name: ['',Validators.required],
-    menu: ['',Validators.required],
-    qty: ['',Validators.required],
-  });
+  // orderForm = this.fb.group({
+  //   name: ['',[Validators.required,Validators.minLength(3)]],
+  //   menu: ['',Validators.required],
+  //   qty: ['',Validators.required],
+  // });
+
+
 
   // coffeeQtyControl = new FormControl('');
 
@@ -37,9 +45,33 @@ export class MainPage {
   qty: number = 0;
   totalPrice: number = 0;
   quantity: Array<number> = [];
+  name: string = '';
+
+  orderForm = new FormGroup({
+    name: new FormControl(''),
+    menu: new FormControl(''),
+    qty: new FormControl('')
+  });
 
   constructor( private fb:FormBuilder){}
 
+  ngOnInit() {
+    this.orderForm = new FormGroup({
+      name: new FormControl(this.name, [
+        Validators.required,
+        // Validators.minLength(4),
+        // this.nameValidator(this.name) // <-- 커스텀 유효성 검사기의 인자는 이렇게 전달합니다.
+        Validators.pattern('[A-Za-z]{5}')
+      ]),
+      menu: new FormControl(''),
+      qty: new FormControl('', Validators.required)
+    });
+  }
+ 
+
+  setName(e: any) {
+    this.name = e.target.value;
+  }
   setMenu(e: any) {
     const menu = e.target.value;
     if (menu !== "no") {
@@ -52,9 +84,13 @@ export class MainPage {
   }
 
 
-  get name() { return this.orderForm.get('name'); }
+  get guest() { return this.orderForm.get('name'); }
+  // get menu() { return this.orderForm.get('me'); }
+  // get qty() { return this.orderForm.get('qty'); }
 
- 
+
+
+
   addCart() {
 
     // this.coffeeQtyControl.setValue(12);
